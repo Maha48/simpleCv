492bb05e58c3606ac3370465d91dc85e7598272b
Maha48 <50016693+Maha48@users.noreply.github.com>
2020-01-15 17:01:41 +0300

    Update and rename y.py to data.py

--- data.py
+++ data.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    user = {'username': "Mahaa", 'bio':"I am Maha from q8  i'm studying in india and i live  in qal3at wadrin"}
+    user = {'username': "Mahaa", 'bio':"I am Maha from KSA  i'm studying in university  and i live  in Riyadh"}
     info = {'phone': '123', 'Email':'A@gmail.com', 'Tel':'999','Address':'424km2' }
 
     return render_template('index.html', user=user,info=info)
